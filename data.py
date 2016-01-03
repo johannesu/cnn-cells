df927c6f7ccc835ecfe8f6320e5068d5a57e43f8
Olof Enqvist <olof.enqvist@chalmers.se>
2016-01-03 10:59:39 +0100

    Using annotated negative samples. Reading image patches for each batch.

--- data.py
+++ data.py
@@ -5,13 +5,47 @@ import skimage.data
 import random
 import numpy as np
 from scipy.ndimage.morphology import distance_transform_edt
+import matplotlib.pyplot as plt
+
+def make_distance_map(image_shape,
+                      centres):
+
+    # Distance transform
+    f = np.ones(image_shape, bool)
+    for idx in xrange(centres.shape[1]):
+        x = int(min(max(0, centres[0, idx]), image_shape[0] - 1))
+        y = int(min(max(0, centres[1, idx]), image_shape[1] - 1))
+
+        f[x, y] = False
+
+    distance_map = distance_transform_edt(f)
+    return distance_map
 
 
-# Can be turned into a generator to speed up things
+def plot_samples(samples,
+                 labels,
+                 img):
+    fig, ax = plt.subplots()
+    ax.imshow(img, interpolation='nearest', cmap=plt.cm.gray)
+
+    neg = [samples[idx] for idx in range(len(samples)) if labels[idx]==0]
+    negplt, = plt.plot([v for u,v in neg],[u for u,v in neg], 'r.', markersize=5, mew=2)
+
+    pos = [samples[idx] for idx in range(len(samples)) if labels[idx]==1]
+    posplt, = plt.plot([v for u,v in pos],[u for u,v in pos], 'g.', markersize=5, mew=2)
+
+    plt.legend([negplt, posplt], ['Negative samples', 'Positive samples'])
+    ax.axis('image')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
+    plt.rcParams['figure.figsize'] = 15,15
+
+
 def generate(data_type="train",
              box_size=29,
-             positive_sample_radius=2,
-             negative_sample_radius=6):
+             sample_radius=2,
+             enable_plotting=False):
 
     root = os.path.abspath('dataset/bloodcells/')
 
@@ -21,9 +55,11 @@ def generate(data_type="train",
     if data_type == "train":
         im_path = root + '/bloodcells_1.png'
         centres_path = root + '/annotations/bloodcells_1.mat'
+        neg_path = root + '/annotations/negative_samples_1.mat'
     elif data_type == "validation":
         im_path = root + '/bloodcells_2.png'
         centres_path = root + '/annotations/bloodcells_2.mat'
+        neg_path = root + '/annotations/negative_samples_2.mat'
     else:
         raise Exception('Data type is either "train" or "validation"')
 
@@ -33,50 +69,50 @@ def generate(data_type="train",
     img = skimage.data.load(im_path)
     img = img.mean(axis=2)
 
-    # Load image centers
+    # Load image centres
     matlab_data = matlabIo.loadmat(centres_path)
     centres = matlab_data['centres']
+    distance_to_positive = make_distance_map(img.shape, centres)
 
-    # Distance transform
-    f = np.ones(img.shape, bool)
-    for idx in xrange(centres.shape[1]):
-        x = int(min(max(0, centres[0, idx]), img.shape[0] - 1))
-        y = int(min(max(0, centres[1, idx]), img.shape[1] - 1))
+    # Load negative samples
+    matlab_data = matlabIo.loadmat(neg_path)
+    negative_centres = matlab_data['centres']
+    distance_to_negative = make_distance_map(img.shape, negative_centres)
 
-        f[x, y] = False
 
-    distance = distance_transform_edt(f)
+    # Positive samples within sample_radius from a cell centre.
+    samples = []
+    labels = []
+    for x in xrange(image_border_margin, img.shape[0] - image_border_margin):
+        for y in xrange(image_border_margin, img.shape[1] - image_border_margin):
+            if (distance_to_positive[x][y] < sample_radius):
+                samples.append((x, y))
+                labels.append(1)
 
-    def sample_box(x, y):
-        margin = int(math.floor(box_size / 2))
-        return img[x-margin:x+margin+1, y-margin:y+margin+1]
+    # Negative samples within sample_radius from negative annotated points
+    nbr_positives = len(samples)
 
-    # Positive samples
-    pos_samples = []
-    for x in xrange(image_border_margin, distance.shape[0] - image_border_margin):
-        for y in xrange(image_border_margin, distance.shape[1] - image_border_margin):
-            if (distance[x][y] < positive_sample_radius):
-                pos_samples.append(sample_box(x, y))
+    for x in xrange(image_border_margin, img.shape[0] - image_border_margin):
+        for y in xrange(image_border_margin, img.shape[1] - image_border_margin):
+            if (distance_to_negative[x][y] < sample_radius):
+                samples.append((x, y))
+                labels.append(0)
 
-    # Negative samples random sample in a band around each cell
-    neg_samples = []
-    while len(pos_samples) > len(neg_samples):
-        x = random.randint(image_border_margin, distance.shape[0] - image_border_margin)
-        y = random.randint(image_border_margin, distance.shape[1] - image_border_margin)
 
-        d = distance[x, y]
+    # Random negative samples at distance > sample_radius from cell centres
+    offset = len(samples)
+    while len(samples) - offset < nbr_positives:
+        x = random.randint(image_border_margin, img.shape[0] - image_border_margin)
+        y = random.randint(image_border_margin, img.shape[1] - image_border_margin)
 
-        if negative_sample_radius > d > positive_sample_radius:
-            neg_samples.append(sample_box(x, y))
+        d = distance_to_positive[x, y]
 
-    labels = np.ones(len(pos_samples))
-    labels = np.append(labels, np.zeros(len(neg_samples)))
+        if d > sample_radius:
+            samples.append((x, y))
+            labels.append(0)
 
-    # Transform to np array
-    samples_list = neg_samples + pos_samples
-    samples = np.zeros((len(samples_list), 1, box_size, box_size))
 
-    for idx, x in enumerate(samples):
-        samples[idx, 0:, :] = samples_list[idx]
+    if enable_plotting:
+        plot_samples(samples, labels, img)
 
     return samples, labels, img
--- minibatch.py
+++ minibatch.py
@@ -1,11 +1,22 @@
 import numpy as np
+import math
 
 
-def iterator(inputs, targets, batchsize=100, shuffle=False):
-    assert len(inputs) == len(targets)
-    indices = np.arange(len(inputs))
+def iterator(samples, targets, img, box_size, batchsize=100, shuffle=False):
+    assert len(samples) == len(targets)
+    
+    def sample_box(x, y):
+        margin = int(math.floor(box_size / 2))
+        return img[x-margin:x+margin+1, y-margin:y+margin+1]
+    
+    indices = np.arange(len(targets))
     if shuffle:
         np.random.shuffle(indices)
-    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
-        excerpt = slice(start_idx, start_idx + batchsize)
-        yield inputs[excerpt], targets[excerpt]
+    for start_idx in range(0, len(targets) - batchsize + 1, batchsize):
+        batch_inputs = np.zeros((batchsize,1,box_size,box_size))
+        batch_targets = np.zeros(batchsize)
+        for kk in range(0, batchsize,1):
+            ind = indices[start_idx+kk]
+            batch_inputs[kk,0:,:] = sample_box(samples[ind][0], samples[ind][1])
+            batch_targets[kk] = targets[ind]
+        yield batch_inputs, batch_targets
